06d528440675ea1effa95389979fd13b786997e6
NaraLuwan <hqcadmin@163.com>
2019-05-12 20:04:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# ImageLSB
+
+- 基于LSB算法隐藏数据到图片中丨从图片中提取隐藏的数据
+- 支持png、bmp等不压缩或无损压缩的图片格式
+
+# Quick Start
+
+- Todo...
---
+++ src/main/java/com/luwan/github/lsb/base/Constants.java
@@ -0,0 +1,10 @@
+package com.luwan.github.lsb.base;
+
+/**
+ * @author luwan
+ * @date 2019/5/12
+ */
+public interface Constants {
+
+  int HIDDEN_MESSAGE_BIT_LENGTH = 32;
+}
